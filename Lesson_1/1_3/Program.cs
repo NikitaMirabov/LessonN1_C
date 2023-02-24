364ac7163622c3a00bb6c3a032a63621cf71ca30
NikitaMirabov <nikitamirabov@gmail.com>
2023-02-24 16:56:39 +0300

    Сделали программу, выдающую множество целых чисел от -x до x

---
+++ Lesson_1/1_3/Program.cs
@@ -0,0 +1,12 @@
+﻿// Программа запрашивает целое число X, а на выходе дает все цедые числа в промежутке от -X до X
+
+string s = Console.ReadLine()!;
+int x = int.Parse(s);
+int y = -x;
+
+while (y <= x)
+{
+    Console.Write(y);
+    Console.Write("; ");
+    y++;
+}
