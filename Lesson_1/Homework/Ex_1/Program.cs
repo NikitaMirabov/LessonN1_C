49c946ac025a71bad7c4433079e67a3eec23c754
NikitaMirabov <nikitamirabov@gmail.com>
2023-02-24 17:15:25 +0300

    Сделал первую хадачу из ДЗ

---
+++ Lesson_1/Homework/Ex_1/Program.cs
@@ -0,0 +1,44 @@
+﻿//  Эта программа на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+
+Console.WriteLine("Привет! Введи 2 числа по очереди, а я проверю и проставлю >, < или =");
+
+string s1 = Console.ReadLine()!;
+double num1 = double.Parse(s1);
+string s2 = Console.ReadLine()!;
+double num2 = double.Parse(s2);
+
+if (num1 == num2)
+{
+   
+    Console.WriteLine("Числа равны!");
+    Console.Write(num1);
+    Console.Write(" = ");
+   
+    Console.Write(num2);
+}
+
+else if (num1 > num2)
+{
+
+    Console.WriteLine("Первое число больше второго!");
+    Console.Write(num1);
+    Console.Write(" > ");
+    Console.Write(num2);
+    
+}
+
+else
+{
+
+   Console.WriteLine("Второе число больше первого!");
+    Console.Write(num1);
+    Console.Write(" < ");
+    Console.Write(num2);
+
+}
+
+
+
+
+
