ff195cfc80df87eeff9cfe120479ef69b769670b
NikitaMirabov <nikitamirabov@gmail.com>
2023-02-24 15:24:51 +0300

    Сам смог сделать задачу с квадратами!!!)

---
+++ Lesson_1/1_1/Program.cs
@@ -0,0 +1,23 @@
+﻿string s1 = Console.ReadLine()!;
+int num1 = int.Parse(s1);
+string s2 = Console.ReadLine()!;
+int num2 = int.Parse(s2);
+
+if (num1 == num2*num2)
+{
+    Console.Write("Yes,");
+    Console.Write(num1);
+    Console.Write("=");
+     Console.Write(num2);
+      Console.Write("*");
+      Console.Write(num2);
+}
+else
+{
+   Console.Write("No,");
+    Console.Write(num1);
+    Console.Write(" not = ");
+     Console.Write(num2);
+      Console.Write("*");
+      Console.Write(num2);
+}
