cd899cd7a6f8f28cf213b18bd4d58b5f5e2792f0
NikitaMirabov <nikitamirabov@gmail.com>
2023-02-25 00:19:52 +0300

    Сделал третье задание из ДЗ

---
+++ Lesson_1/Homework/Ex_3/Program.cs
@@ -0,0 +1,22 @@
+﻿//  Эта программа принимает число и выдаёт, является ли число чётным.
+
+Console.WriteLine("Привет, впиши число, а я проверю, четное ли оно четным");
+
+string s1 = Console.ReadLine()!;
+double num = double.Parse(s1);
+
+if (num % 2 == 0)
+{
+   
+    Console.Write(num);
+    Console.Write(" - Четное!) ");
+
+}
+
+else
+{
+
+    Console.Write(num);
+    Console.Write(" - Не делится на 2 без остатка!) ");
+
+}
