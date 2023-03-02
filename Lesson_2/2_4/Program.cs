6983ddfbf51ff8a8ad4dac14882564684960407f
NikitaMirabov <nikitamirabov@gmail.com>
2023-03-02 20:51:34 +0300

    Сделали 4ую задачу, программа проверяет крастность введеного чисела к 7 и 23

---
+++ Lesson_2/2_4/Program.cs
@@ -0,0 +1,13 @@
+﻿/* Программа проверяет кратно ли введеное число одновременно 7 и 23 */
+
+int a = int.Parse(Console.ReadLine()!);
+
+void Div(int a)
+{
+
+    if (a % 7 == 0 & a % 23 ==0) Console.WriteLine("Кратно");
+    else Console.WriteLine("Не кратно");
+
+}
+
+Div(a);
