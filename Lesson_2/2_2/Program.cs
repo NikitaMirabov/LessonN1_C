0f74d397ea3bcd302c7b6f8608893507bd16bf1a
NikitaMirabov <nikitamirabov@gmail.com>
2023-03-02 18:11:03 +0300

    Сделали 2ую задачу, программа находит большее число из состава двухзначного числа

---
+++ Lesson_2/2_2/Program.cs
@@ -0,0 +1,17 @@
+﻿/* Напишите программу, которая выводит случайное число из отрезка [10, 99] 
+и показывает наибольшую цифру числа */
+int MaxNum(int num)
+{
+    Console.WriteLine(num);
+    int num1 = num/10;
+    int num2 = num % 10;
+    if (num1 > num2) return num1;
+    return num2;
+
+}
+
+int result = MaxNum(new Random().Next(10, 99));
+
+
+
+Console.WriteLine(result);
