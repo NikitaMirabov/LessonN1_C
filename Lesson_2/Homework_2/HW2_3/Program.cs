6c067c203a94679893265253e1c79ff7dcd3f99a
NikitaMirabov <nikitamirabov@gmail.com>
2023-03-15 23:42:44 +0300

    Сделал  задачу 2 и 3 из ДЗ

---
+++ Lesson_2/Homework_2/HW2_3/Program.cs
@@ -0,0 +1,32 @@
+﻿/* Задача 3: Напишите программу, которая принимает на вход цифру, 
+обозначающую день недели, и проверяет, является ли этот день выходным.
+- 6 -> да
+- 7 -> да
+- 1 -> нет */
+
+void TakeNum(int num)
+{
+   
+    if (num > 0 & num < 6) 
+    { 
+         
+        Console.WriteLine(num + " - Рабочий!"); 
+    
+    }    
+    else if (num > 5 & num < 8) 
+    { 
+         
+        Console.WriteLine(num + " - Выходной!"); 
+    
+    }    
+    else
+    {
+
+        Console.WriteLine(num + "?! - Такого дня нет!");
+
+    }
+
+}
+
+TakeNum(int.Parse(Console.ReadLine()!));
+ 
