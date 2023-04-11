f893391e3845fb282264ee30e23115f5fc619ad8
NikitaMirabov <nikitamirabov@gmail.com>
2023-04-11 21:31:52 +0300

    Семинар №8

---
+++ Lesson_8/8_0/Program.cs
@@ -0,0 +1,49 @@
+﻿// 1. Задайте двумерный массив. Напишите программу,
+// которая поменяет местами первую и последнюю строку массива.
+
+void Print(int[,] arr)
+{
+    int row = arr.GetLength(0);
+    int column = arr.GetLength(1);
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+            Console.Write($" {arr[i, j]} ");
+        Console.WriteLine();
+    }
+}
+
+int[,] MassNums(int row, int column, int from, int to)
+{
+    int[,] arr = new int[row, column];
+
+    for (int i = 0; i < row; i++)
+        for (int j = 0; j < column; j++)
+            arr[i, j] = new Random().Next(from, to);
+    return arr;
+}
+
+void ReplaceIndex(int[,] array)
+{
+    int ArrayRow = array.GetLength(0);
+    int ArrayColumn = array.GetLength(1);
+
+    for (int j = 0; j < ArrayColumn; j++)
+    {
+        (array[0, j], array[ArrayRow - 1, j]) = (array[ArrayRow - 1, j], array[0, j]);
+    }
+}
+
+
+int row_num = int.Parse(Console.ReadLine()!);
+int column_num = int.Parse(Console.ReadLine()!);
+int start = int.Parse(Console.ReadLine()!);
+int stop = int.Parse(Console.ReadLine()!);
+
+int[,] mass = MassNums(row_num, column_num, start, stop);
+
+Print(mass);
+ReplaceIndex(mass);
+Console.WriteLine();
+Print(mass);
---
+++ Lesson_8/8_1/Program.cs
@@ -0,0 +1,61 @@
+﻿// 2. Задайте двумерный массив. Напишите программу,
+// которая заменяет строки на столбцы. В случае, если это невозможно,
+// программа должна вывести сообщение для пользователя.
+
+void Print(int[,] arr)
+{
+    int row = arr.GetLength(0);
+    int column = arr.GetLength(1);
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+            Console.Write($" {arr[i, j]} ");
+        Console.WriteLine();
+    }
+}
+
+int[,] MassNums(int row, int column, int from, int to)
+{
+    int[,] arr = new int[row, column];
+
+    for (int i = 0; i < row; i++)
+        for (int j = 0; j < column; j++)
+            arr[i, j] = new Random().Next(from, to);
+    return arr;
+}
+
+bool Check(int row, int column)
+{
+    bool result = row != column;
+    return result;
+}
+
+string RowToColumn(int[,] array)
+{
+    if (Check(array.GetLength(0), array.GetLength(1))) return "Нет!";
+
+    for (int i = 1; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            (array[i, j], array[j, i]) = (array[j, i], array[i, j]);
+        }
+    }
+    Print(array);
+    return "Да!";
+}
+
+
+int row_num = int.Parse(Console.ReadLine()!);
+int column_num = int.Parse(Console.ReadLine()!);
+int start = int.Parse(Console.ReadLine()!);
+int stop = int.Parse(Console.ReadLine()!);
+
+int[,] mass = MassNums(row_num, column_num, start, stop);
+
+Print(mass);
+Console.WriteLine();
+string result = RowToColumn(mass);
+Console.WriteLine(result);
+
