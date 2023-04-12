55b0bfcd304cc114d47b79c8c67ed213143a2025
NikitaMirabov <nikitamirabov@gmail.com>
2023-04-12 23:59:09 +0300

    Семинар №9 + 2 задачи из Д/З

---
+++ Lesson_9/Homework_9/HW9_1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет все чётные
+// натуральные числа в промежутке от M до N с помощью рекурсии.
+
+void FromTo(int M, int N)
+{
+    if (M == N ) return;
+    if (M % 2 == 0 )
+        Console.Write($"{M}; ");
+    FromTo(M+1, N);
+}
+
+Console.WriteLine("От какого числа идем?");
+int m = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("До какого числа идем?");
+int n = int.Parse(Console.ReadLine()!);
+
+FromTo(m, n + 1);
---
+++ Lesson_9/Homework_9/HW9_2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму
+// натуральных элементов в промежутке от M до N с помощью рекурсии.
+
+void FromTo(int M, int N, int Sum)
+{
+    if (M < N)
+    {
+        Console.Write($"Сумма натуральных чисел в промежутке: {Sum} ");
+        return;
+    }
+    Sum = Sum + N++;
+    FromTo(M, N, Sum);
+}
+
+Console.WriteLine("От какого числа идем?");
+int n = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("До какого числа идем?");
+int m = int.Parse(Console.ReadLine()!);
+
+FromTo(m, n, 0);
