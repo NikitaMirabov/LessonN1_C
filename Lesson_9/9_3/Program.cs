1ef5b5084808a905eca8a24a47ba6645cbbda7d0
NikitaMirabov <nikitamirabov@gmail.com>
2023-04-12 22:20:06 +0300

    Семинар №9. Рекурсия

---
+++ Lesson_9/9_3/Program.cs
@@ -0,0 +1,13 @@
+﻿// 3. Напишите программу, которая будет принимать на вход число
+// и возвращать сумму его цифр. Использовать рекурсию.
+
+int Sum(int N)
+{
+    if(N == 0) return 0;
+    return Sum(N / 10) + N % 10;
+}
+
+int n = int.Parse(Console.ReadLine()!);
+int res = Sum(n);
+
+Console.Write(res);
---
+++ Lesson_9/9_4/Program.cs
@@ -0,0 +1,14 @@
+﻿// 4. Напишите программу, которая на вход принимает два числа A и B,
+// и возводит число А в целую степень B с помощью рекурсии.
+
+int Pow(int A, int B)
+{
+    if(B == 0) return 1;
+    return A* Pow(A, B-1);
+}
+
+int a = int.Parse(Console.ReadLine()!);
+int b = int.Parse(Console.ReadLine()!);
+
+int res = Pow(a, b);
+Console.Write(res);
